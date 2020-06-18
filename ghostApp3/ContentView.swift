738cd99aa0fabf2a1c9c659b3dce7220c8cadb35
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-06-18 09:21:41 -0700

    Initial Commit

---
+++ ghostApp3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ghostApp3
+//
+//  Created by W Wessels on 6/18/20.
+//  Copyright © 2020 W Wessels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
